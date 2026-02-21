1ba14bf5f105b8ee37b1103f863ba319d28a258d
cplepage <lepage.charlesphilippe@gmail.com>
2026-02-20 23:23:27 -0500

    export functions

--- index.ts
+++ index.ts
@@ -1,17 +1,46 @@
 import fs from "fs";
+import path from "path";
 import oxide, { extract } from "@esm.sh/oxide-wasm";
 import { compile } from "tailwindcss";
 import lightningcss, { transform } from "lightningcss-wasm";
 
-await oxide(fs.promises.readFile("./node_modules/@esm.sh/oxide-wasm/pkg/oxide_wasm_bg.wasm"));
-await lightningcss("./node_modules/lightningcss-wasm/lightningcss_node.wasm");
+let oxideInit: Promise<void> | null = null;
+let lightningcssInit: Promise<void> | null = null;
+let tailwindcssBaseDir: string | null = null;
+
+export function init(wasm: {
+    oxide: string,
+    tailwindcss: string,
+    lightningcss: string
+}) {
+    if (oxideInit === null) {
+        oxideInit = oxide(fs.promises.readFile(wasm.oxide));
+    }
+
+    if (lightningcssInit === null) {
+        lightningcssInit = lightningcss(wasm.lightningcss);
+    }
+
+    if (tailwindcssBaseDir === null) {
+        tailwindcssBaseDir = wasm.tailwindcss;
+    }
+
+    return Promise.all([
+        oxideInit,
+        lightningcssInit
+    ]);
+}
 
 async function loadStylesheet(id: string, base: string) {
+    if (tailwindcssBaseDir === null) {
+        throw new Error("tailwindcss has not been initialized");
+    }
+
     if (id === "tailwindcss") {
         return {
             path: "virtual:tailwindcss/index.css",
             base,
-            content: fs.readFileSync("./node_modules/tailwindcss/index.css", "utf-8")
+            content: await fs.promises.readFile(path.join(tailwindcssBaseDir, "index.css"), "utf-8")
         };
     } else if (
         id === "tailwindcss/theme" ||
@@ -21,7 +50,7 @@ async function loadStylesheet(id: string, base: string) {
         return {
             path: "virtual:tailwindcss/theme.css",
             base,
-            content: fs.readFileSync("./node_modules/tailwindcss/theme.css", "utf-8")
+            content: await fs.promises.readFile(path.join(tailwindcssBaseDir, "theme.css"), "utf-8")
         };
     } else if (
         id === "tailwindcss/utilities" ||
@@ -31,26 +60,21 @@ async function loadStylesheet(id: string, base: string) {
         return {
             path: "virtual:tailwindcss/utilities.css",
             base,
-            content: fs.readFileSync("./node_modules/tailwindcss/utilities.css", "utf-8")
+            content: await fs.promises.readFile(path.join(tailwindcssBaseDir, "utilities.css"), "utf-8")
         };
     }
 
     throw new Error(`The browser build does not support @import for "${id}"`);
 }
 
-const code = `<h1 class="text-3xl font-bold underline bg-red-500">
-  Hello world!
-</h1>`;
-
-const candidate = extract(code);
-
-const compiler = await compile("@import 'tailwindcss';", { loadStylesheet });
-
-const css = compiler.build(candidate);
-
-const result = transform({
-    filename: "input.css",
-    code: new TextEncoder().encode(css),
-})
-
-fs.writeFileSync("./output.css", result.code);
+export async function build(outfile: string, files: string[]) {
+    const contents = await Promise.all(files.map(file => fs.promises.readFile(file, "utf-8")));
+    const candidate = contents.map(content => extract(content)).flat();
+    const compiler = await compile("@import 'tailwindcss';", { loadStylesheet });
+    const css = compiler.build(candidate);
+    const result = transform({
+        filename: "input.css",
+        code: new TextEncoder().encode(css)
+    })
+    return fs.promises.writeFile(outfile, result.code);
+}
